461f508c6c099399fb3b7f3e1f566f7048028fb8
skreebydba <skreebydba@users.noreply.github.com>
2018-10-23 10:01:26 -0500

    Update RemoveOldFiles.ps1
    Added file extension parameter

--- RemoveOldFiles.ps1
+++ RemoveOldFiles.ps1
@@ -10,6 +10,8 @@
   Give another example of how to use it
   .PARAMETER filepath 
   The file path to search for old files.
+  .PARAMETER extension
+  The file extension to delete.
   .PARAMETER days
   Number of days to check for files.
   #>
@@ -28,13 +30,20 @@
     ValueFromPipelineByPropertyName=$True,
       HelpMessage='Enter the number of days you want to go back to delete files?')]
     [Alias('days')]
-    [ValidateLength(3,255)]
-    [string[]]$numberofdays
+    [int]$numberofdays,
+    [Parameter(Mandatory=$False,
+    ValueFromPipeline=$True,
+    ValueFromPipelineByPropertyName=$True,
+      HelpMessage='What file extension do you want to search for?')]
+    [Alias('ext')]
+    [ValidateLength(2,15)]
+    [string[]]$extension
   )
 
 
   process {
-
-        Get-ChildItem $filepath -Recurse -File | Where CreationTime -lt  (Get-Date).AddDays($numberofdays) | Out-GridView -PassThru | Remove-Item -Force;
+        
+        $extension = "*" + $extension
+        Get-ChildItem $filepath -Recurse -File -Filter $extension | Where CreationTime -lt  (Get-Date).AddDays($numberofdays) | Out-GridView -PassThru | Remove-Item -Force;
       }
     }
